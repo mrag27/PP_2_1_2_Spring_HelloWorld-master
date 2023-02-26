6f6e3964f5551817be8d16e1a6749b5c6e1c7582
Bulgakov Egor <egor_mrag1999@,ail.ru>
2023-02-26 22:31:09 +0300

    2.1.3

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String message;
+    public void Cat() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
